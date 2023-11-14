c61b2c4a867f41944d43bdcd5822b96bb2a55694
mustaphabousildev <bousilmustapha@gmail.com>
2023-11-14 11:27:54 +0100

    order usecases finish

--- src/useCases/products/index.js
+++ src/useCases/products/index.js
@@ -1,8 +1,10 @@
 const addProductUseCase=require('./addProduct.useCase')
 const getProductByIdUseCase=require('./getProductById.useCase')
 const updateProducUseCase=require('./updateProduct.useCase')
+const deleteProductUseCase=require('./deleteProduct.useCase')
 module.exports={
     addProductUseCase,
     getProductByIdUseCase,
-    updateProducUseCase
+    updateProducUseCase,
+    deleteProductUseCase
 }
